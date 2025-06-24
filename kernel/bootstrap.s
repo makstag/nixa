1518b6191fa0fc5c586d2076f505582a72d06deb
makstag <mstitovforgit@gmail.com>
2025-06-24 18:29:13 +0300

    remove init bss

--- kernel/bootstrap.s
+++ kernel/bootstrap.s
@@ -8,19 +8,11 @@ bootstrap:
 		.cfi_startproc
 		.cfi_undefined ra
 
-		/* Zero-out BSS */
-		la a4, bss_start
-		la a5, bss_end
-.Lbss_zero:
-		sd zero, (a4)
-		add a4, a4, PTR_SIZE
-		blt a4, a5, .Lbss_zero
-
 		/* Disable and clear all interrupts */
 		csrw sie, zero
 		csrw sip, zero
 
-		
+
 
 		la sp, stack_top
 
--- kernel/riscv.h
+++ kernel/riscv.h
@@ -2,7 +2,6 @@
 #define RISCV_H
 
 #define PTR_SIZE 0x8
-#define OFFSET 0x800
 #define PAGE_SIZE 0x1000
 #define STACK_SIZE 0x2000
 #define KERNEL_BASE 0x80200000
