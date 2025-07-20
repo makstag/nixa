d1cd49fa4a8aaac317397c311e1474fa9dc124fa
makstag <mstitovforgit@gmail.com>
2025-07-20 19:00:20 +0300

    init pages

--- kernel/boot.s
+++ kernel/boot.s
@@ -12,6 +12,8 @@
 .equ PTE_WRITE, 1 << 2
 .equ PTE_EXECUTE, 1 << 3
 
+.equ STACK_SIZE, 0x8000
+
 .section .text.boot, "ax"
 .align 3
 .global boot
@@ -21,7 +23,7 @@ boot:
 		.cfi_startproc
 		.cfi_undefined ra
 
-.macro PPM, reg, pt
+.macro PPN, reg, pt
 		la \reg, \pt
 		srli \reg, \reg, PAGE_SHIFT
 .endm
@@ -40,8 +42,44 @@ boot:
 		sw \ppn, 0(t0)
 .endm
 
+		csrw sie, zero
+		csrw sip, zero 
 
+		PPN t2, L2_CODE 
+		PTE_SET L3_KERNEL, KERNEL_BASE, 3, t2, PTE_VALID 
 
+		PPN t2, KERNEL_BASE 
+		PTE_SET L2_CODE, KERNEL_BASE, 2, t2, PTE_VALID | PTE_EXECUTE | PTE_READ | PTE_WRITE 
+
+		PPN t2, L2_DATA 
+		PTE_SET L3_KERNEL, DATA, 3, t2, PTE_VALID 
+
+		PPN t2, DATA 
+		PTE_SET L2_DATA, DATA, 2, t2, PTE_VALID | PTE_READ | PTE_WRITE 
+
+		la a3, END  
+		addi a3, a3, STACK_SIZE
+		li a2, PAGE_SIZE 
+		cssr a4, mhartid
+		addi a4, a4, 1
+		mul a2, a2, a4
+		add a3, a3, a2
+
+		PPN t2, L1_STACK 
+		PTE_SET L2_DATA, a3, 2, t2, PTE_VALID 
+
+		PPN t2, a3
+		PTE_SET L1_STACK, a3, 1, t2, PTE_VALID | PTE_READ | PTE_WRITE 
+
+		li t1, SATP_BITS 
+		slli t1, t1, SATP_SHIFT 
+		PPN t0, L3_KERNEL 
+		or t0, t0, t1
+		csrw satp, t0
+
+		la sp, a3
+
+		call main
 		.cfi_endproc
 
 .macro DEFINE_PAGE, name
@@ -54,4 +92,8 @@ boot:
 
 .endm
 
-DEFINE_PAGE root_table
+DEFINE_PAGE L3_KERNEL
+DEFINE_PAGE L2_CODE
+DEFINE_PAGE L2_DATA
+DEFINE_PAGE L1_STACK
+
