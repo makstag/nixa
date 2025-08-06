bb637d7b9561122d9f80d6a3d3d4c16119e1aa42
Maks Titov <MSTitovForGit@gmail.com>
2025-08-06 19:53:31 +0300

    Update common.h

--- include/common.h
+++ include/common.h
@@ -5,10 +5,6 @@ typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
 typedef unsigned long uint64;
-typedef char int8;
-typedef short int16;
-typedef int int32;
-typedef long int64;
 
 #define PAGE_SIZE 4096
 #define SIZEOF_PTR 8
